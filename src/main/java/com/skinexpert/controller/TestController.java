e5778cbb20785109d57c1e73d3b7969f37257410
mikhailKolomiets <mikhail.kolomiets@gmail.com>
2018-08-08 18:52:58 +0300

    servlets

--- src/main/java/com/skinexpert/controller/TestController.java
+++ src/main/java/com/skinexpert/controller/TestController.java
@@ -1,18 +1,20 @@
 package com.skinexpert.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
- * Test rest controller. will be deleted any time...
+ * Test servlet. will be deleted any time...
  */
-@RestController
-@RequestMapping(value = "/rest")
-public class TestController {
 
-    @RequestMapping(value = "/any", method = RequestMethod.GET)
-    public String anyStringPage() {
-        return "test ok";
+@WebServlet(urlPatterns = "/test")
+public class TestController extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/result.html");
     }
 }
