07a9369623e4a5d923481f0e996e3e2f3f512691
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2018-10-22 23:41:32 +0300

    Added log4j to class and refactored code.

--- src/main/java/com/skinexpert/controller/component/FindComponent.java
+++ src/main/java/com/skinexpert/controller/component/FindComponent.java
@@ -3,14 +3,16 @@ package com.skinexpert.controller.component;
 import com.google.gson.Gson;
 import com.skinexpert.entity.Component;
 import com.skinexpert.service.ComponentService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,23 +20,35 @@ import java.util.List;
  */
 @WebServlet(urlPatterns = "/find-component/*")
 public class FindComponent extends HttpServlet {
+    private Logger logger;
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void init() {
+        this.logger = LogManager.getLogger(this.getClass());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 
         resp.setContentType("application/json; charset = utf8");
-        req.setCharacterEncoding("utf8");
-
-        String name = req.getPathInfo().substring(1);
-        List<Component> components = null;
-        if (name.length() > 0) {
-            try {
-                components = new ComponentService().findNameBySubstring(name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            req.setCharacterEncoding("utf8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Error while setCharset process.", e);
+            throw new RuntimeException(e);
         }
 
+        String name = req.getPathInfo().substring(1);
+        logger.info("Trying to get components list");
+        List<Component> components = (name.length() > 0 ?
+                new ComponentService().findNameBySubstring(name) : Collections.emptyList());
+
         String outMessage = new Gson().toJson(components);
-        resp.getWriter().write(outMessage);
+        try {
+            resp.getWriter().write(outMessage);
+        } catch (IOException e) {
+            logger.error("Error while creating response message.", e);
+            throw new RuntimeException(e);
+        }
     }
 }
