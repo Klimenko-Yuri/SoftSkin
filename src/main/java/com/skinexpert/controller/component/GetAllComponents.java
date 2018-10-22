0e0874a828dbe6f5c039c4bee2b34842f4069441
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2018-10-22 23:46:27 +0300

    Added log4j to class and refactored code.

--- src/main/java/com/skinexpert/controller/component/GetAllComponents.java
+++ src/main/java/com/skinexpert/controller/component/GetAllComponents.java
@@ -3,13 +3,15 @@ package com.skinexpert.controller.component;
 import com.google.gson.Gson;
 import com.skinexpert.entity.Component;
 import com.skinexpert.service.ComponentService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 /**
@@ -17,15 +19,33 @@ import java.util.List;
  */
 @WebServlet(urlPatterns = "/get-all-component")
 public class GetAllComponents extends HttpServlet {
+    private Logger logger;
+    private ComponentService componentService;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json; charset = utf8");
-        req.setCharacterEncoding("utf8");
+    public void init() {
+        this.logger = LogManager.getLogger(this.getClass());
+        this.componentService = new ComponentService();
+    }
 
-        List<Component> componentList = new ComponentService().getAll();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("application/json; charset = utf8");
+        try {
+            req.setCharacterEncoding("utf8");
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Error while setCharset process.", e);
+            throw new RuntimeException(e);
+        }
+
+        List<Component> componentList = componentService.getAll();
 
         String outMessage = new Gson().toJson(componentList);
-        resp.getWriter().write(outMessage);
+        try {
+            resp.getWriter().write(outMessage);
+        } catch (IOException e) {
+            logger.error("Error while creating response message.", e);
+            throw new RuntimeException(e);
+        }
     }
 }
